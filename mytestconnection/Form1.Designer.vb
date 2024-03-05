e60ce827b95a8fb4b84bd7dbc534d3e92fcfdf5a
Sumeet Patel <Sumeet Patel@DESKTOP-HO2PUH5>
2024-03-06 02:11:31 +0530

    Add project files.

---
+++ mytestconnection/Form1.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.insbtn = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(77, 46)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(237, 46)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 1
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(365, 46)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(200, 20)
+        Me.DateTimePicker1.TabIndex = 2
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(325, 84)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(240, 150)
+        Me.DataGridView1.TabIndex = 3
+        '
+        'insbtn
+        '
+        Me.insbtn.Location = New System.Drawing.Point(102, 97)
+        Me.insbtn.Name = "insbtn"
+        Me.insbtn.Size = New System.Drawing.Size(75, 23)
+        Me.insbtn.TabIndex = 4
+        Me.insbtn.Text = "Insert"
+        Me.insbtn.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.insbtn)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents insbtn As Button
+End Class
---
+++ mytestconnection/Form1.vb
@@ -0,0 +1,43 @@
+﻿Imports System.Configuration
+Imports System.Data.Common
+Imports System.Data.OleDb
+Public Class Form1
+    Dim connection As OleDbConnection
+    Dim ds As DataSet
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim connection_string As String = ConfigurationManager.ConnectionStrings("c_string").ConnectionString
+        connection = New OleDbConnection(connection_string)
+        filldatagrid()
+
+    End Sub
+
+    Private Sub filldatagrid()
+        ds = New DataSet
+        Dim dtp As New OleDbDataAdapter("select * from vbr", connection)
+        dtp.Fill(ds, "m_table")
+        DataGridView1.DataSource = ds.Tables("m_table")
+
+    End Sub
+
+    Private Sub insbtn_Click(sender As Object, e As EventArgs) Handles insbtn.Click
+        Try
+            connection.Open()
+            Dim qry As String
+            qry = "insert into vbr values" & "(?,?,to_date(?,'YYYY-MM-DD'))"
+            Dim command As New OleDbCommand(qry, connection)
+            command.Parameters.AddWithValue("?", CInt(TextBox1.Text))
+            command.Parameters.AddWithValue("?", TextBox2.Text)
+            command.Parameters.AddWithValue("?", DateTimePicker1.Value.ToString("yyyy-MM-dd"))
+
+            Dim insrow = command.ExecuteNonQuery()
+            connection.Close()
+            If (insrow >= 1) Then
+                MessageBox.Show("row inserted bro")
+                filldatagrid()
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+End Class
